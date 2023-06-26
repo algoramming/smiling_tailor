53bc8c8e93255895ac6fc49e6b6f14c1cc081e0b
Md. Sabik Alam Rahat <sabikrahat72428@gmail.com>
2023-06-27 01:01:12 +0600

    responsive remove

--- lib/src/config/is.under.min.size.dart
+++ lib/src/config/is.under.min.size.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
 
 bool isUnderMinSize(Size size, [Size minimumSize = const Size(1170, 550)]) =>
-    false; // TODO:
-    // size.height < minimumSize.height || size.width < minimumSize.width;
+    size.height < minimumSize.height || size.width < minimumSize.width;
