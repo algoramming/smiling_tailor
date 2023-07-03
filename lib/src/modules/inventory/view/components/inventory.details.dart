accc5345c0afb224f7945145570ab5bf46eedbc5
Md. Sabik Alam Rahat <sabikrahat72428@gmail.com>
2023-07-04 01:56:51 +0600

    inventory financial tab close

--- lib/src/modules/inventory/view/components/inventory.details.dart
+++ lib/src/modules/inventory/view/components/inventory.details.dart
@@ -91,7 +91,7 @@ class _TrxTable extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: DefaultTabController(
-        length: 3,
+        length: 2,
         child: NestedScrollView(
           floatHeaderSlivers: true,
           headerSliverBuilder: (_, __) => [
@@ -101,7 +101,7 @@ class _TrxTable extends StatelessWidget {
                 dividerColor: Colors.transparent,
                 labelStyle: context.theme.textTheme.labelLarge,
                 tabs: const [
-                  Tab(text: 'Financials'),
+                  // Tab(text: 'Financials'),
                   Tab(text: 'Inventories'),
                   Tab(text: 'Orders'),
                 ],
@@ -110,7 +110,7 @@ class _TrxTable extends StatelessWidget {
           ],
           body: TabBarView(
             children: [
-              _FinancialsTab(notifier),
+              // _FinancialsTab(notifier),
               _InventoriesTab(notifier),
               _OrdersTab(notifier),
             ],
@@ -121,32 +121,32 @@ class _TrxTable extends StatelessWidget {
   }
 }
 
-class _FinancialsTab extends StatelessWidget {
-  const _FinancialsTab(this.notifier);
+// class _FinancialsTab extends StatelessWidget {
+//   const _FinancialsTab(this.notifier);
 
-  final InventoryProvider notifier;
+//   final InventoryProvider notifier;
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        _TrxList(notifier,
-            condition: (t) =>
-                t.isGoods == false &&
-                t.fromType.isNotOrder &&
-                t.toType.isNotOrder),
-        const SizedBox(height: 10),
-        _TotalSummaryFinancials(
-          notifier,
-          (t) =>
-              t.isGoods == false &&
-              t.fromType.isNotOrder &&
-              t.toType.isNotOrder,
-        ),
-      ],
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         _TrxList(notifier,
+//             condition: (t) =>
+//                 t.isGoods == false &&
+//                 t.fromType.isNotOrder &&
+//                 t.toType.isNotOrder),
+//         const SizedBox(height: 10),
+//         _TotalSummaryFinancials(
+//           notifier,
+//           (t) =>
+//               t.isGoods == false &&
+//               t.fromType.isNotOrder &&
+//               t.toType.isNotOrder,
+//         ),
+//       ],
+//     );
+//   }
+// }
 
 class _InventoriesTab extends StatelessWidget {
   const _InventoriesTab(this.notifier);
@@ -206,185 +206,187 @@ class _TrxList extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Expanded(
       child: ref.watch(inventoryTrxsProvider(notifier.selectedInventory!)).when(
-          loading: () => const LoadingWidget(withScaffold: false),
-          error: (err, _) => KErrorWidget(error: err),
-          data: (_) {
-            final noti = ref.watch(
-                inventoryTrxsProvider(notifier.selectedInventory!).notifier);
-            final trxs = noti.trxList.where(condition).toList();
-            return trxs.isEmpty
-                ? const KDataNotFound(msg: 'No Transaction Found!')
-                : SlidableAutoCloseBehavior(
-                    child: ListView.builder(
-                      itemCount: trxs.length,
-                      itemBuilder: (_, i) {
-                        final trx = trxs[i];
-                        final kColor =
-                            trx.trxType.isCredit ? Colors.red : Colors.green;
-                        return Card(
-                          child: KListTile(
-                            key: ValueKey(trx.id),
-                            isSystemGenerated: trx.isSystemGenerated,
-                            canEdit: false,
-                            onDeleteTap: () async =>
-                                await trxDeletePopup(context, trx),
-                            onLongPress: () async =>
-                                await copyToClipboard(context, trx.id),
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 12.0, vertical: 5.0),
-                            leading: AnimatedWidgetShower(
-                              padding: 3.0,
-                              size: 35.0,
-                              child: trx.modifiers,
-                            ),
-                            title: RichText(
-                              text: TextSpan(
-                                children: [
-                                  TextSpan(
-                                    text: trx.fromName,
-                                    style: context.text.titleSmall,
-                                    recognizer: TapGestureRecognizer()
-                                      ..onTap = () async =>
-                                          await copyToClipboard(
-                                              context, trx.fromId),
-                                  ),
-                                  WidgetSpan(
-                                    child: Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 6.0),
-                                      child: RotatedBox(
-                                        quarterTurns:
-                                            trx.trxType.isDebit ? 0 : 1,
-                                        child: Icon(
-                                          Icons.arrow_outward_rounded,
-                                          size: 16,
-                                          color: kColor,
+            loading: () => const LoadingWidget(withScaffold: false),
+            error: (err, _) => KErrorWidget(error: err),
+            data: (_) {
+              final noti = ref.watch(
+                  inventoryTrxsProvider(notifier.selectedInventory!).notifier);
+              final trxs = noti.trxList.where(condition).toList();
+              return trxs.isEmpty
+                  ? const KDataNotFound(msg: 'No Transaction Found!')
+                  : SlidableAutoCloseBehavior(
+                      child: ListView.builder(
+                        itemCount: trxs.length,
+                        itemBuilder: (_, i) {
+                          final trx = trxs[i];
+                          final kColor =
+                              trx.trxType.isCredit ? Colors.red : Colors.green;
+                          return Card(
+                            child: KListTile(
+                              key: ValueKey(trx.id),
+                              isSystemGenerated: trx.isSystemGenerated,
+                              canEdit: false,
+                              onDeleteTap: () async =>
+                                  await trxDeletePopup(context, trx),
+                              onLongPress: () async =>
+                                  await copyToClipboard(context, trx.id),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 12.0, vertical: 5.0),
+                              leading: AnimatedWidgetShower(
+                                padding: 3.0,
+                                size: 35.0,
+                                child: trx.modifiers,
+                              ),
+                              title: RichText(
+                                text: TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: trx.fromName,
+                                      style: context.text.titleSmall,
+                                      recognizer: TapGestureRecognizer()
+                                        ..onTap = () async =>
+                                            await copyToClipboard(
+                                                context, trx.fromId),
+                                    ),
+                                    WidgetSpan(
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 6.0),
+                                        child: RotatedBox(
+                                          quarterTurns:
+                                              trx.trxType.isDebit ? 0 : 1,
+                                          child: Icon(
+                                            Icons.arrow_outward_rounded,
+                                            size: 16,
+                                            color: kColor,
+                                          ),
                                         ),
                                       ),
                                     ),
+                                    TextSpan(
+                                      text: trx.toName,
+                                      style: context.text.titleSmall,
+                                      recognizer: TapGestureRecognizer()
+                                        ..onTap = () async =>
+                                            await copyToClipboard(
+                                                context, trx.toId),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: crossStart,
+                                children: [
+                                  Text(
+                                    trx.createdDate,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 1,
+                                    style: context.text.labelMedium,
                                   ),
-                                  TextSpan(
-                                    text: trx.toName,
-                                    style: context.text.titleSmall,
-                                    recognizer: TapGestureRecognizer()
-                                      ..onTap = () async =>
-                                          await copyToClipboard(
-                                              context, trx.toId),
-                                  ),
+                                  if (trx.description != null)
+                                    Text(
+                                      trx.description!,
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 2,
+                                      style: context.text.labelSmall!.copyWith(
+                                          fontWeight: FontWeight.normal),
+                                    ),
                                 ],
                               ),
-                            ),
-                            subtitle: Column(
-                              crossAxisAlignment: crossStart,
-                              children: [
-                                Text(
-                                  trx.createdDate,
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 1,
-                                  style: context.text.labelMedium,
-                                ),
-                                if (trx.description != null)
-                                  Text(
-                                    trx.description!,
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 2,
-                                    style: context.text.labelSmall!.copyWith(
-                                        fontWeight: FontWeight.normal),
-                                  ),
-                              ],
-                            ),
-                            trailing: TweenAnimationBuilder(
-                              curve: Curves.easeOut,
-                              duration: kAnimationDuration(0.5),
-                              tween: Tween<double>(begin: 0, end: trx.amount),
-                              builder: (_, double x, __) {
-                                return Tooltip(
-                                  message: trx.isGoods ? '' : x.formattedFloat,
-                                  child: Text(
-                                    !trx.isGoods
-                                        ? x.formattedCompat
-                                        : '${x.toInt()} ${trx.unit?.symbol ?? '??'}',
-                                    style: context.text.labelLarge!.copyWith(
-                                      color: kColor,
+                              trailing: TweenAnimationBuilder(
+                                curve: Curves.easeOut,
+                                duration: kAnimationDuration(0.5),
+                                tween: Tween<double>(begin: 0, end: trx.amount),
+                                builder: (_, double x, __) {
+                                  return Tooltip(
+                                    message:
+                                        trx.isGoods ? '' : x.formattedFloat,
+                                    child: Text(
+                                      !trx.isGoods
+                                          ? x.formattedCompat
+                                          : '${x.toInt()} ${trx.unit?.symbol ?? '??'}',
+                                      style: context.text.labelLarge!.copyWith(
+                                        color: kColor,
+                                      ),
                                     ),
-                                  ),
-                                );
-                              },
+                                  );
+                                },
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                    ),
-                  );
-          }),
-    );
-  }
-}
-
-class _TotalSummaryFinancials extends ConsumerWidget {
-  const _TotalSummaryFinancials(this.notifier, this.condition);
-
-  final InventoryProvider notifier;
-  final bool Function(PktbsTrx trx) condition;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    ref.watch(inventoryTrxsProvider(notifier.selectedInventory!));
-    final noti =
-        ref.watch(inventoryTrxsProvider(notifier.selectedInventory!).notifier);
-    final trxs = noti.rawTrxs.where(condition).toList();
-    final total = notifier.selectedInventory?.amount.toString().toDouble ?? 0.0;
-    final adjusted = trxs.isEmpty
-        ? 0.0
-        : trxs.fold<double>(
-            0.0,
-            (p, c) =>
-                p +
-                (c.trxType.isCredit ? c.amount : 0.0) -
-                (c.trxType.isDebit ? c.amount : 0.0));
-    final due = total - adjusted;
-    final kColor = due.isNegative ? Colors.green : Colors.red;
-    return Card(
-      color: context.theme.dividerColor.withOpacity(0.2),
-      child: ListTile(
-        leading: AnimatedWidgetShower(
-          padding: 3.0,
-          size: 35.0,
-          child: Card(
-            color: kColor.withOpacity(0.2),
-            shape: roundedRectangleBorder10,
-            child: SvgPicture.asset(
-              'assets/svgs/performance-overlay.svg',
-              colorFilter: kColor.toColorFilter,
-            ),
+                          );
+                        },
+                      ),
+                    );
+            },
           ),
-        ),
-        title: Text(
-          'Total due till now ${appSettings.getDateTimeFormat.format(DateTime.now())}',
-        ),
-        subtitle: Text(
-          'Total Purchase: ${total.formattedFloat} and Given: ${adjusted.formattedFloat}',
-        ),
-        trailing: TweenAnimationBuilder(
-          curve: Curves.easeOut,
-          duration: kAnimationDuration(0.5),
-          tween: Tween<double>(begin: 0, end: due),
-          builder: (_, double x, __) {
-            return Tooltip(
-              message: x.formattedFloat,
-              child: Text(
-                x.formattedCompat,
-                style: context.text.labelLarge!.copyWith(
-                  color: kColor,
-                ),
-              ),
-            );
-          },
-        ),
-      ),
     );
   }
 }
+
+// class _TotalSummaryFinancials extends ConsumerWidget {
+//   const _TotalSummaryFinancials(this.notifier, this.condition);
+
+//   final InventoryProvider notifier;
+//   final bool Function(PktbsTrx trx) condition;
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     ref.watch(inventoryTrxsProvider(notifier.selectedInventory!));
+//     final noti =
+//         ref.watch(inventoryTrxsProvider(notifier.selectedInventory!).notifier);
+//     final trxs = noti.rawTrxs.where(condition).toList();
+//     final total = notifier.selectedInventory?.amount.toString().toDouble ?? 0.0;
+//     final adjusted = trxs.isEmpty
+//         ? 0.0
+//         : trxs.fold<double>(
+//             0.0,
+//             (p, c) =>
+//                 p +
+//                 (c.trxType.isCredit ? c.amount : 0.0) -
+//                 (c.trxType.isDebit ? c.amount : 0.0));
+//     final due = total - adjusted;
+//     final kColor = due.isNegative ? Colors.green : Colors.red;
+//     return Card(
+//       color: context.theme.dividerColor.withOpacity(0.2),
+//       child: ListTile(
+//         leading: AnimatedWidgetShower(
+//           padding: 3.0,
+//           size: 35.0,
+//           child: Card(
+//             color: kColor.withOpacity(0.2),
+//             shape: roundedRectangleBorder10,
+//             child: SvgPicture.asset(
+//               'assets/svgs/performance-overlay.svg',
+//               colorFilter: kColor.toColorFilter,
+//             ),
+//           ),
+//         ),
+//         title: Text(
+//           'Total due till now ${appSettings.getDateTimeFormat.format(DateTime.now())}',
+//         ),
+//         subtitle: Text(
+//           'Total Purchase: ${total.formattedFloat} and Given: ${adjusted.formattedFloat}',
+//         ),
+//         trailing: TweenAnimationBuilder(
+//           curve: Curves.easeOut,
+//           duration: kAnimationDuration(0.5),
+//           tween: Tween<double>(begin: 0, end: due),
+//           builder: (_, double x, __) {
+//             return Tooltip(
+//               message: x.formattedFloat,
+//               child: Text(
+//                 x.formattedCompat,
+//                 style: context.text.labelLarge!.copyWith(
+//                   color: kColor,
+//                 ),
+//               ),
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 class _TotalSummaryInventories extends ConsumerWidget {
   const _TotalSummaryInventories(this.notifier, this.condition);
